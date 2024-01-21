ec602204d020be11c25de0c2615c18b80f01c67c
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-01-21 17:46:00 -0300

    fix: updating the skeleton and format of the total value of the student card

--- frontend/src/pages/app/dashboard/card-student-total.tsx
+++ frontend/src/pages/app/dashboard/card-student-total.tsx
@@ -3,6 +3,7 @@ import { Activity } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 export const CardStudentTotal = ({ total }: { total: number | undefined }) => {
+  const formattedNumber = total?.toLocaleString()
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -13,7 +14,7 @@ export const CardStudentTotal = ({ total }: { total: number | undefined }) => {
       </CardHeader>
       <CardContent className="space-y-1">
         <span className="text-2xl font-bold tracking-tighter text-teal-700 dark:text-teal-500">
-          {total}
+          {formattedNumber}
         </span>
       </CardContent>
     </Card>
--- frontend/src/pages/app/schools/school-table-skeleton.tsx
+++ frontend/src/pages/app/schools/school-table-skeleton.tsx
@@ -15,16 +15,16 @@ export function SchoolTableSkeleton() {
           </Button>
         </TableCell>
         <TableCell>
-          <Skeleton className="h-4 w-[172px]" />
+          <Skeleton className="h-4 w-[172px]  max-sm:w-[50px]" />
         </TableCell>
         <TableCell>
-          <Skeleton className="h-4 w-[148px]" />
+          <Skeleton className="h-4 w-[600px] max-lg:w-[300px] max-sm:w-[50px]" />
         </TableCell>
         <TableCell>
-          <Skeleton className="h-4 w-[110px]" />
+          <Skeleton className="h-4 w-[110px] max-sm:w-[50px]" />
         </TableCell>
         <TableCell>
-          <Skeleton className="h-4 w-[110px]" />
+          <Skeleton className="h-4 w-[110px] max-sm:w-[50px]" />
         </TableCell>
       </TableRow>
     )
